3e816ac1b5d9339d5d1057daee11dba94665350d
Bram Adams <3282661+bramses@users.noreply.github.com>
2022-08-11 10:05:42 -0500

    index

--- dist/index.js
+++ dist/index.js
@@ -8,8 +8,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const url = process.argv[2];
-const title = process.argv.slice(3).join(" ");
+const url = process.argv[2].split(" ")[0];
+const title = process.argv[2].split(" ").slice(1).join(" ");
+if (!title || !url) {
+    console.log("Usage: node index.ts <url> <title>");
+    process.exit(1);
+}
 const getVideoUrl = (url) => {
     return url.split("/").pop();
 };
@@ -24,12 +28,12 @@ const slugify = (text) => {
 };
 const main = (title, url) => __awaiter(void 0, void 0, void 0, function* () {
     const videoId = getVideoUrl(url);
-    title = title = title.replace(/Highlight\:/g, "").trim();
-    let finalHTML = `---
-title: '${title}'
+    const cleanTitle = (title = title.replace(/Highlight\:/g, "").trim());
+    const finalHTML = `---
+title: "${cleanTitle}"
 excerpt: "Highlight clipped from stream"
 ogImageUrl: 'https://imagedelivery.net/FVn4Kw8Yr8auy8XS7UL4RA/23df1849-f98d-479b-6fa4-8721f93e7700/public'
-slug: '${slugify(title)}'
+slug: '${slugify(cleanTitle)}'
 published: false
 state: 'twitch'
 ---
@@ -40,4 +44,4 @@ state: 'twitch'
 `;
     console.log(finalHTML);
 });
-main("Highlight: Screen cap the knowledge work you're already doing", "https://www.twitch.tv/videos/1557607195");
+main(title, url);
